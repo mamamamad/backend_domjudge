b3a1b9e455878cf9d62d4855c93770bb485de76c
mamamamad <mamadhub8@gmail.com>
2025-11-16 13:07:26 +0330

    test

--- src/controllers/teamController.mjs
+++ src/controllers/teamController.mjs
@@ -98,7 +98,7 @@ export async function createTeam(req, res) {
       public_description: teamData.teamname?.trim() || "",
       location: "null",
       members: "null",
-      group_ids: ["Participants"],
+      group_ids: ["8"],
     };
     let createData = {};
 
